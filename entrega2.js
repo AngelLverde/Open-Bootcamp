c1cc61b4aa6ef4cad151416d3aaf50a38049e7ab
Angel-Verde <verdeangel95@hotmail.com>
2022-11-08 20:33:00 -0300

    first commit

---
+++ entrega2.js
@@ -0,0 +1,13 @@
+//SEGUNDA ENTREGA//
+
+let nombre = "Angel";
+let edad = 25; 
+let eresDesarrollador = true;
+const nacimiento = new Date ("october 14 1995");
+
+const libro = {
+    titulo: "el Psicoanalista",
+    autor:"John Katzenbach",
+   url: "https://www.todostuslibros.com/libros/el-psicoanalista_978-84-9070-665-7",
+    fecha: 2002,
+}
---
+++ index.js
@@ -0,0 +1,8 @@
+//esto es un comentario//
+
+
+
+console.log ("esta es la puerta de entrada al proyecto");
+
+const ahora = new Date();
+console.log(ahora);
